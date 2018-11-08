101af5b05c987ef27f1034f888175bec55229d02
Hu Chao Xiang <charles@HumatoMacBook-Pro.local>
2018-11-07 19:46:51 -0500

    123

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -1,30 +1,48 @@
 import React, { Component } from "react";
 import Userbadge from "../components/UserBadge";
 import ProjectCard from "../components/ProjectCard";
+import API from "../API/API.js"
 
 class DashBoard extends Component {
     state = {
-        user: {
-            firstName: "Neha",
-            lastName: "Pathak",
-            projects: ["123", "456", "789"],
-            score: 455
-        },
-        projects: [
-            {
-                name: "Project 1",
-                rating: 4.5
-            },
-            {
-                name: "Project 2",
-                rating: 4
-            },
-            {
-                name: "Project 3",
-                rating: 5
-            }
-        ]
+        
+        lastName: "",
+        projects: [],
+        id: "",
+        user:{
+            firstName: "",
+        }
     }
+    componentWillMount() {
+        this.loadUser();
+    }
+
+    loadUser = () => {
+        API.getUser(this.id)
+            .then(res => {
+                this.setState({
+                    user:{firstName:res.data.firstName},
+                    lastName:res.data.lastName,
+                    projects:res.data.projects,
+                })
+            }
+            ).catch(err => console.log(err));
+    }
+    // projects: [
+    //     {
+    //         name: "Project 1",
+    //         rating: 4.5
+    //     },
+    //     {
+    //         name: "Project 2",
+    //         rating: 4
+    //     },
+    //     {
+    //         name: "Project 3",
+    //         rating: 5
+    //     }
+    // ]
+
     render() {
         return (
             <div className="row jumbotron jumbotron-fluid">
