31aee6ef0db92caf9ae828f8161411f0c91d305b
Hu Chao Xiang <charles@HumatoMacBook-Pro.local>
2018-11-09 23:52:00 -0500

    working create-user page

--- client/src/components/CreateUser/CreateUser.js
+++ client/src/components/CreateUser/CreateUser.js
@@ -10,37 +10,35 @@ class createUser extends Component {
         Password: "",
         Email: "",
         Mobile: "",
-        imageUrl:null,
+        imageUrl: null,
     }
     handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
+
+        if (event.target.name !== "imageUrl") {
+            this.setState({
+                [event.target.name]: event.target.value
+            });
+        }
+        else {
+            this.setState({
+                [event.target.name]: event.target.files[0]
+            });
+        }
     };
-    handleImgFile=event=>{
-        
-        this.setState({
-            imageUrl:event.target.files[0],
-        })
-        console.log("files")
-        console.log(event.target.files[0])
-        // console.log(event)
-    }
+
     handleFormSubmit = event => {
         event.preventDefault();
         console.log(this.state.imageUrl)
         if (this.state.userName && this.state.Password && this.state.lastName && this.state.firstName) {
-            API.createUser({
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                username: this.state.userName,
-                password: this.state.Password,
-                email: this.state.Email,
-                cell: this.state.Mobile,
-                imageUrl: this.state.imageUrl,
-            }).then(res=>{console.log(res)}
-               )
+            const data = new FormData();
+            data.append('firstName', this.state.firstName);
+            data.append('lastName', this.state.lastName);
+            data.append('password', this.state.Password);
+            data.append('email', this.state.Email);
+            data.append('cell', this.state.Mobile);
+            data.append('imageUrl', this.state.imageUrl);
+            API.createUser(data).then(res => { console.log(res) }
+            )
                 .catch(err => console.log(err));
         }
     }
@@ -48,57 +46,64 @@ class createUser extends Component {
 
     render() {
         return (
-            <div className="input-group mb-3">
-                <form>
-                    <div className="input-group-prepend">
-                        <label>
-                            User Name:
-            <input type="text" name="userName" value={this.state.userName}  onChange={this.handleInputChange} />
-                        </label>
+            <form>
+                <div className="form-row">
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="User Name"
+                            name="userName" value={this.state.userName} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            First Name:
-               <input type="text" name="firstName" value={this.state.firstName}  onChange={this.handleInputChange} />
-                        </label>
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="First Name"
+                            name="firstName" value={this.state.firstName} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            Last Name:
-               <input type="text" name="lastName" value={this.state.lastName}  onChange={this.handleInputChange} />
-                        </label>
+                    <div className="col">
+                        <input type="text" name="lastName" className="form-control" placeholder="Last name"
+                            value={this.state.lastName} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            Password:
-               <input type="text" name="Password" value={this.state.Password}  onChange={this.handleInputChange}/>
-                        </label>
+                    <div className="col">
+                        <input type="text" name="lastName" className="form-control" placeholder="Last name"
+                            value={this.state.lastName} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            Email:
-               <input type="text" name="Email" value={this.state.Email}  onChange={this.handleInputChange}/>
-                        </label>
+                    <div className="col">
+                        <input type="text" name="Email" className="form-control" placeholder="Email"
+                            value={this.state.Mobile} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            Mobile (Optional):
-               <input type="text" name="Mobile" value={this.state.Mobile}  onChange={this.handleInputChange}/>
-                        </label>
+                    <div className="col">
+                        <input type="file" name="imageUrl" className="form-control" placeholder="Profile Pic(Optional)"
+                            value={this.state.Mobile} onChange={this.handleInputChange} />
                     </div>
-                    <div className="input-group-prepend">
-                        <label>
-                            iMage URL (Optional):
-               <input 
-               type="file" 
-               name="imageUrl" 
-            //    value={this.state.imageUrl} 
-               onChange={this.handleImgFile}/>
-                        </label>
-                    </div>
-                    <button  type="submit" className="btn btn-primary" onClick={this.handleFormSubmit}>Create user</button>
-                </form>
-            </div>
+                </div>
+                <br />
+                <div className="form-group">
+                    <input
+                        type="email"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email (required)"
+                        name="Email"
+                        value={this.state.Email}
+                        onChange={this.handleInputChange} />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password (required)"
+                        name="Password"
+                        value={this.state.Password}
+                        onChange={this.handleInputChange} />
+                </div>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={!(this.state.email && this.state.password)}
+                    onClick={this.handleFormSubmit}>
+                    SUBMIT
+                </button>
+                &nbsp;or <a className="btn btn-secondary" href="/">BACK TO LOGIN</a>
+
+            </form>
         )
     }
 }
